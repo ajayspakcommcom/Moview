043ccd40041045818a6e236313e3d10fbee158fa
Ajay Mahavir Vishwakarma <ajayspak@spakcomm.com>
2024-06-27 15:42:55 +0530

    changes

--- src/navigation/HomeNavigation.tsx
+++ src/navigation/HomeNavigation.tsx
@@ -3,7 +3,6 @@ import { View, Text, StyleSheet, Button } from 'react-native';
 import { createNativeStackNavigator, NativeStackNavigationOptions } from '@react-navigation/native-stack';
 import Colors from '../styles/Colors';
 import HomeScreen from '../screens/Home/HomeScreen';
-import TestScreen from '../screens/Test/TestScreen';
 import DetailScreen from '../screens/Home/DetailScreen';
 import { TextAlign } from '../styles/TextAlignmentUtils';
 
--- src/screens/Home/DetailScreen.tsx
+++ src/screens/Home/DetailScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text, StyleSheet, Button, ScrollView, ImageSourcePropType } from 'react-native';
+import { View, Text, StyleSheet, Button, ScrollView, ImageSourcePropType, Image } from 'react-native';
 import { StackNavigationProp, StackScreenProps } from '@react-navigation/stack';
 import { useRoute, useNavigation, ParamListBase, NavigationProp, RouteProp } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Ionicons';
@@ -7,6 +7,9 @@ import Colors from '../../styles/Colors';
 import { MovieItem } from '../../types/Movie';
 import { findMovieById } from '../../utils/Common';
 import { MovieDataList } from '../../utils/Data';
+import Fonts from '../../styles/Fonts';
+import { Rating, AirbnbRating } from 'react-native-ratings';
+
 
 type Props = {
 
@@ -18,11 +21,19 @@ const DetailScreen: React.FC<Props> = ({ }) => {
 
     const navigation: NavigationProp<ParamListBase> = useNavigation();
     const route: RouteProp<{ params: { id: string } }> = useRoute();
-
+    const [detailData, setDetailData] = React.useState<Partial<MovieItem>>({});
 
     React.useLayoutEffect(() => {
 
         const movie = findMovieById(movieList, route.params.id);
+
+        setDetailData(prevState => ({
+            ...prevState,
+            id: movie?.id,
+            title: movie?.title,
+            image: movie?.image,
+            videoUrl: movie?.videoUrl
+        }));
 
         const backButtonHandler = () => {
             navigation.navigate('HomeScreen');
@@ -45,9 +56,36 @@ const DetailScreen: React.FC<Props> = ({ }) => {
         return console.log('');
     }, []);
 
+    const ratingCompleted = (rating: number) => {
+        console.log("Rating is: " + rating)
+    }
+
     return (
         <ScrollView style={styles.container}>
-            <View style={styles.box}></View>
+            <View style={styles.header}>
+                {detailData.image && <Image source={detailData.image} style={styles.img} />}
+            </View>
+
+            <View style={styles.detailText}>
+                <Text style={styles.detailHeading}>Black Panther</Text>
+                <View style={styles.ratingWrapper}>
+                    <AirbnbRating
+                        count={5}
+                        reviews={["Bad", "Meh", "OK", "Good", "Jesus"]}
+                        defaultRating={1}
+                        size={25}
+                        onFinishRating={ratingCompleted}
+                        showRating={false}
+                    />
+                </View>
+            </View>
+
+            <View style={styles.genreWrapper}>
+                <View style={styles.genreItem}><Text style={styles.genreText}>Action</Text></View>
+                <View style={styles.genreItem}><Text style={styles.genreText}>Adventures</Text></View>
+                <View style={styles.genreItem}><Text style={styles.genreText}>Sic-Fi</Text></View>
+            </View>
+
         </ScrollView>
     );
 };
@@ -55,16 +93,56 @@ const DetailScreen: React.FC<Props> = ({ }) => {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: 'pink'
+        flex: 1
     },
-    box: {
+    header: {
+        flex: 1,
         width: '100%',
-        height: 500,
-        backgroundColor: 'red',
+        paddingHorizontal: 15
+    },
+    img: {
+        width: '100%',
+        height: 300,
+        resizeMode: 'cover'
+    },
+    detailText: {
+        paddingVertical: 15,
+        paddingHorizontal: 15
+    },
+    detailHeading: {
+        color: Colors.whiteColor,
+        fontFamily: Fonts.Family.Bold,
+        fontSize: Fonts.Size.Medium + 4,
+        textTransform: 'uppercase'
+    },
+
+    ratingWrapper: {
+        paddingVertical: 5,
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start'
+    },
+
+    genreWrapper: {
+        flexDirection: 'row',
+        paddingHorizontal: 15
+    },
+    genreItem: {
+        marginRight: 15,
+        paddingVertical: 2,
+        paddingHorizontal: 15,
         borderWidth: 1,
-        borderColor: 'yellow'
+        borderColor: Colors.whiteColor,
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    genreText: {
+        color: Colors.whiteColor,
+        fontFamily: Fonts.Family.Medium
     }
+
+
+
 });
 
 

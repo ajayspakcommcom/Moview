3dd0c1834b9665ca02bda66d74ce83a3d5bb4ed0
Ajay Mahavir Vishwakarma <ajayspak@spakcomm.com>
2024-07-01 17:11:27 +0530

    changes

--- src/screens/Profile/HomeScreen.tsx
+++ src/screens/Profile/HomeScreen.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, ScrollView } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import Colors from '../../styles/Colors';
 import { useRoute, useNavigation, ParamListBase, NavigationProp, RouteProp } from '@react-navigation/native';
 import { useAuth } from '../../context/AuthContext';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import Fonts from '../../styles/Fonts';
 
 
 type Props = {
@@ -26,26 +28,134 @@ const HomeScreen: React.FC<Props> = ({ }) => {
 
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Search Screen</Text>
-            {/* <Button title="Go to Detail Screen" onPress={onSaveHandler} /> */}
-            <Button title="Go to Detail Screen" onPress={onLogoutHandler} />
-        </View>
+        <ScrollView style={styles.container}>
+
+            <View style={styles.headerWrapper}>
+                <View style={styles.header}>
+                    <View style={styles.headerContent}>
+                        <View style={styles.userTextIcon}>
+                            <View style={styles.userIcon}>
+                                <Icon name={'user-alt'} size={80} color={Colors.tabBgColor} onPress={() => console.log('Ram...')} style={styles.icon} />
+                            </View>
+                            <View style={styles.userTextWrapper}>
+                                <Text style={styles.name}>Shiv Kar</Text>
+                                <Text style={styles.critic}>Film Critic</Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+                <View style={styles.followerWrapper}>
+                    <View style={styles.movies}>
+                        <Text style={styles.follText}>200</Text>
+                        <Text style={styles.follText}>Movies</Text>
+                    </View>
+                    <View style={styles.followers}>
+                        <Text style={styles.follText}>179</Text>
+                        <Text style={styles.follText}>Followers</Text>
+                    </View>
+                    <View style={styles.following}>
+                        <Text style={styles.follText}>179</Text>
+                        <Text style={styles.follText}>Following</Text>
+                    </View>
+                </View>
+            </View>
+
+        </ScrollView>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        // justifyContent: 'flex-start',
+        // alignItems: 'center',
+        // backgroundColor: Colors.darkBackgroudColor,
+    },
+    headerWrapper: {
+
+    },
+    header: {
+        width: '100%',
+        minHeight: 300,
         justifyContent: 'center',
-        alignItems: 'center',
+        alignContent: 'center',
+        flexWrap: 'wrap',
+        borderWidth: 1,
+        borderBottomColor: 'red'
+    },
+    headerContent: {
+        width: 200,
+        minHeight: 200,
+        justifyContent: 'center',
+        alignContent: 'center',
+        flexWrap: 'wrap'
+    },
+    userTextIcon: {
+        width: '100%',
+        justifyContent: 'center',
+        alignContent: 'center',
+        flexWrap: 'wrap',
+        flexDirection: 'row'
+    },
+    userIcon: {
         backgroundColor: Colors.whiteColor,
+        width: 150,
+        height: 150,
+        borderRadius: 150,
+        justifyContent: 'center',
+        alignContent: 'center'
     },
-    text: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 20,
+    userTextWrapper: {
+        width: '100%',
+        height: 50,
     },
+    name: {
+        marginTop: 10,
+        color: Colors.whiteColor,
+        textAlign: 'center',
+        fontFamily: Fonts.Family.Bold,
+        fontSize: Fonts.Size.Medium + 10,
+        lineHeight: 30
+    },
+    critic: {
+        color: Colors.whiteColor,
+        textAlign: 'center',
+        fontFamily: Fonts.Family.Medium,
+        fontSize: Fonts.Size.Medium + 2,
+        lineHeight: 25
+    },
+    icon: {
+        textAlign: 'center',
+        lineHeight: 150
+    },
+
+    followerWrapper: {
+        width: '100%',
+        //height: 100,
+        //backgroundColor: 'pink',
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    movies: {
+        marginRight: 15,
+        borderRightColor: Colors.whiteColor,
+        padding: 15
+    },
+    followers: {
+        marginRight: 15,
+        borderRightColor: Colors.whiteColor,
+        padding: 15
+    },
+    following: {
+        padding: 15
+    },
+    follText: {
+        textAlign: 'center',
+        fontFamily: Fonts.Family.Medium,
+        fontSize: Fonts.Size.Medium,
+        color: Colors.whiteColor
+    }
+
 });
 
 export default HomeScreen;

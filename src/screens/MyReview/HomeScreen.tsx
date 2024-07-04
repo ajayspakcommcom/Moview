de67da8a83c7d054a9d9cfa7e3b973058faa51db
Ajay Mahavir Vishwakarma <ajayspak@spakcomm.com>
2024-07-04 11:08:18 +0530

    changes

--- src/screens/MyReview/HomeScreen.tsx
+++ src/screens/MyReview/HomeScreen.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
+import ReviewList from '../../components/ReviewList/ReviewList';
 
 type Props = {
 
@@ -14,7 +15,9 @@ const HomeScreen: React.FC<Props> = () => {
 
     return (
         <>
-            <View style={styles.container}></View>
+            <View style={styles.container}>
+                <ReviewList />
+            </View>
         </>
     );
 };
@@ -22,7 +25,7 @@ const HomeScreen: React.FC<Props> = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'red'
+        paddingVertical: 15
     }
 });
 
--- src/screens/Search/HomeScreen.tsx
+++ src/screens/Search/HomeScreen.tsx
@@ -2,21 +2,49 @@ import * as React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import Colors from '../../styles/Colors';
+import { Searchbar } from 'react-native-paper';
+import { MovieItem } from '../../types/Movie';
+import { MovieDataList } from '../../utils/Data';
+import MovieList from '../../components/MovieList/MovieList';
 
 type Props = {
-    navigation: StackNavigationProp<any>; // Replace with appropriate navigation prop type
+    navigation: StackNavigationProp<any>;
 };
 
-const HomeScreen: React.FC<Props> = ({ navigation }) => {
+const movieList: MovieItem[] = [...MovieDataList];
 
-    const goToDetailScreen = () => {
-        navigation.navigate('DetailScreen');
+const HomeScreen: React.FC<Props> = () => {
+
+    const [searchQuery, setSearchQuery] = React.useState('');
+    const [movies, setMovies] = React.useState(movieList);
+    const [fiteredMovies, setFilteredMovies] = React.useState(movies);
+    const [movie, setMovie] = React.useState<MovieItem[]>([]);
+
+    const onChangeSearch = (query: string): void => {
+        setSearchQuery(query);
     };
+
+    const onClearHandler = () => {
+        setFilteredMovies(movies);
+    };
+
+    React.useLayoutEffect(() => {
+        setMovie(movieList);
+        return () => setMovie([]);
+    }, [movie]);
 
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Search Screen</Text>
-            <Button title="Go to Detail Screen" onPress={goToDetailScreen} />
+
+            <View style={styles.searchWrapper}>
+                <Searchbar placeholder="Search Movie" onChangeText={onChangeSearch} value={searchQuery} onClearIconPress={onClearHandler} />
+            </View>
+
+            <View style={styles.movieList}>
+                {movie.length >= 0 && <MovieList />}
+                {movie.length <= 0 && <View><Text style={styles.text}>Emypty List</Text></View>}
+            </View>
+
         </View>
     );
 };
@@ -24,15 +52,21 @@ const HomeScreen: React.FC<Props> = ({ navigation }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: Colors.whiteColor,
+        padding: 15
     },
+    searchWrapper: {
+        flexGrow: 1,
+        paddingVertical: 15
+    },
+    movieList: {
+        flexGrow: 1
+    },
+
     text: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 20,
-    },
+        color: Colors.whiteColor,
+        textAlign: 'center',
+        marginTop: '50%'
+    }
 });
 
 export default HomeScreen;

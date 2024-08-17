e8b9891b52dbb37d2ae5eaa44a51578b88061ed5
Ajay Spak <ajay@spakcomm.com>
2024-08-17 19:02:26 +0530

    changes

--- src/utils/ShowImageMap.ts
+++ src/utils/ShowImageMap.ts
@@ -10,9 +10,20 @@ const ShowImageMap: ShowMap = {
     'taarak-mehta-kaulta-chasma-poster': require(`../assets/images/shows/taarak-mehta-kaulta-chasma-poster.png`),
     'paatal-lok-poster': require(`../assets/images/shows/paatal-lok-poster.png`),
     'the-family-man-poster': require(`../assets/images/shows/the-family-man-poster.png`),
-    'chandrakanta-posture': require(`../assets/images/shows/chandrakanta-posture.png`),
+    'chandrakanta-poster': require(`../assets/images/shows/chandrakanta-poster.png`),
     'balika-vadhu-poster': require(`../assets/images/shows/balika-vadhu-poster.png`),
-    'kapil-sharma-show': require(`../assets/images/shows/kapil-sharma-show.png`)
+    'kapil-sharma-show-poster': require(`../assets/images/shows/kapil-sharma-show-poster.png`),
+    'mirzapur-poster': require(`../assets/images/shows/mirzapur-poster.png`),
+    'sasural-simar-ka-poster': require(`../assets/images/shows/sasural-simar-ka-poster.png`),
+    'na-bole-tum-na-maine-kuch-kaha-poster': require(`../assets/images/shows/na-bole-tum-na-maine-kuch-kaha-poster.png`),
+    'udaan-poster': require(`../assets/images/shows/udaan-poster.png`),
+    'bhabhi-ji-ghar-pe-hain': require(`../assets/images/shows/bhabhi-ji-ghar-pe-hain.png`),
+    'pyar-ki-yeh-ek-kahani': require(`../assets/images/shows/pyar-ki-yeh-ek-kahani.png`),
+
 };
 
 export default ShowImageMap;
+
+
+
+

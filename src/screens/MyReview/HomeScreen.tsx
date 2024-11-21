081591842ee9cc125328623ffecb764874e97019
Ajay Spak <ajay@spakcomm.com>
2024-11-21 19:00:01 +0530

    changes

--- src/screens/MyReview/HomeScreen.tsx
+++ src/screens/MyReview/HomeScreen.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
-import { StyleSheet, View} from 'react-native';
+import { StyleSheet, View, Text, Pressable} from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
-
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/index';
 import { MovieReviewResponse, ShowReviewResponse } from '../../models/MyReview';
 import { useAuth } from '../../context/AuthContext';
+import Colors from '../../styles/Colors';
+import Fonts from '../../styles/Fonts';
+
 
 
 const MyReviewList = React.lazy(() => import('../../components/MyReviewList/MyReviewList'));
@@ -17,7 +19,8 @@ type Props = {
 
 const HomeScreen: React.FC<Props> = ({navigation}) => {
 
-    const { userDetail } = useAuth();
+    const { userDetail, logout } = useAuth();
+    
 
     const { data: moviewReviewsData } = useSelector((state: RootState) => state.myMovieReview);    
     const { data: showReviewsData } = useSelector((state: RootState) => state.myShowReview);
@@ -25,9 +28,17 @@ const HomeScreen: React.FC<Props> = ({navigation}) => {
     const [moviewReviews, setMoviewReviews] = React.useState<MovieReviewResponse[]>([]);
     const [showReviews, setShowReviews] = React.useState<ShowReviewResponse[]>([]);
 
-    const getMovieShowReview = React.useCallback(() => {             
-        setMoviewReviews(moviewReviewsData);
-        setShowReviews(showReviewsData);
+    const getMovieShowReview = React.useCallback(() => {
+        
+        if(userDetail.role === 'guest') {
+            setMoviewReviews([]);
+            setShowReviews([]);
+        } else {            
+            setMoviewReviews(moviewReviewsData);
+            setShowReviews(showReviewsData);
+        }
+        
+        
     }, [moviewReviewsData, showReviewsData]); 
 
       useFocusEffect(
@@ -37,11 +48,25 @@ const HomeScreen: React.FC<Props> = ({navigation}) => {
         }, [getMovieShowReview]) 
     );
 
+    const navigationHandler = () => {
+        logout();
+    };
+
     return (              
             <View style={styles.container}>                
-                <React.Suspense fallback={<Loading />}>
-                    <MyReviewList userItem={userDetail} isUser={false} movies={moviewReviews} shows={showReviews} />
-                </React.Suspense>
+                {userDetail.role !== 'guest' && 
+                    <React.Suspense fallback={<Loading />}>
+                         <MyReviewList userItem={userDetail} isUser={false} movies={moviewReviews} shows={showReviews} />
+                    </React.Suspense>
+                }
+
+                {userDetail.role === 'guest' && 
+                    <View style={styles.withoutLoginWrapper}>
+                        <Pressable style={styles.pressableBtn} onPress={navigationHandler}>
+                            <Text style={styles.pressableText}>Please Login</Text>
+                        </Pressable>
+                    </View>
+                }
             </View>        
     );
 };
@@ -50,6 +75,19 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingVertical: 15
+    }, 
+    withoutLoginWrapper: {
+        flex: 1,        
+        justifyContent:'center', 
+        alignItems:'center'
+    }, 
+    pressableBtn: {
+        
+    }, 
+    pressableText: {
+        color:Colors.whiteColor, 
+        fontFamily:Fonts.Family.Bold, 
+        fontSize:Fonts.Size.Medium
     }
 });
 

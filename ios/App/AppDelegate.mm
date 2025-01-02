a97dae03f08e979655ead8b6e5b1886f059c00a6
Ajay Spak <ajayspakcommcom>
2025-01-02 15:19:31 +0530

    changes

--- ios/App/AppDelegate.mm
+++ ios/App/AppDelegate.mm
@@ -11,6 +11,7 @@
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
+  [NSThread sleepForTimeInterval:3];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 

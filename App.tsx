424b0c33a3cebf7b86a72ef0fedd4f107a17e273
Ajay Spak <ajayspakcommcom>
2025-01-04 19:05:04 +0530

    changes

--- App.tsx
+++ App.tsx
@@ -98,7 +98,7 @@ function App(): React.JSX.Element {
               })}
             />
 
-              <Stack.Screen name="Test1" component={Test1} options={{ animationEnabled: false }} /> 
+              {/* <Stack.Screen name="Test1" component={Test1} options={{ animationEnabled: false }} />  */}
             {/*<Stack.Screen name="Test2" component={Test2} options={{ animationEnabled: false }} /> */}
             <Tab.Screen name="Search" component={SearchNavigation} options={TabNavigationOptions.Search} />
 
